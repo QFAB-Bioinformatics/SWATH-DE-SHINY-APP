95b06546e12844674baad90b47d2652ed2d451a6
Vincent Poisson <v.poisson@imb.uq.edu.au>
2017-05-04 13:00:42 +1000

    possibility to use experiment design file

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ require(ggplot2)
 require(ggrepel)
 require(svglite)
 ## function performing the differential expression analysis
-DEprot <- function(data,nbCond=NULL, rep,condName=NULL, normalization="SCALING"){
+DEprot <- function(data,nbCond=NULL, rep,condName=NULL, normalization="MEAN"){
   
   ##applying different normalization depending on the normalization parameter 
   normalization=toupper(normalization)
@@ -73,93 +73,262 @@ DEprot <- function(data,nbCond=NULL, rep,condName=NULL, normalization="SCALING")
   return(results)
 }
 
+norm <- function(data, normalization="MEAN"){
+  
+  if(!is.data.frame(data) & !is.matrix(data)){print("the data must be a data.frame or a matrix")}
+  
+  normalization=toupper(normalization)
+  if(identical(normalization, "MEAN")){
+    data<-scale(log2(data))
+  }
+  else if(identical(normalization, "QUANTILE")){
+    data <- log2(data)
+    nomCol <- colnames(data)
+    data <- as.data.frame(normalize.quantiles(as.matrix(data)), row.names = row.names(data))
+    colnames(data) <- nomCol
+  }
+  else if(identical(normalization, "MEDIAN")){
+    data<-log2(data)
+    for (x in 1:length(data)){
+      data[,x]<-(data[,x]-median(data[,x],na.rm = T))/(mad(data[,x],na.rm = T))
+    }
+  }
+  else{
+    stop("normalization must be : 'MEAN', 'QUANTILE' or 'MEDIAN'")
+  }
+  return(data)
+}
+
+test.stat <- function(data, stat = "t.test" , design , contrast){
+  if(!is.data.frame(data) & !is.matrix(data)){print("the data must be a data.frame or a matrix")}
+  if(!is.data.frame(design) & !is.matrix(design)){print("the experience design must be a matrix generated from model.matrix")}
+  if(!is.data.frame(contrast) & !is.matrix(contrast)){print("the contrast matrix must be  a matrix generated from makeContrasts()")}
+  
+  
+  nbComp<-ncol(contrast)
+  fc <- list()
+  p.value <- list()
+  adjust.p.value<-list()
+  listResults<-list()
+  
+  for (i in 1:nbComp){
+    
+    op<-row.names(contrast)[contrast[,i] == 1 ]
+    ref<-row.names(contrast)[contrast[,i] == -1 ]
+    
+    samplesop <- row.names(design)[row(as.matrix(design[,op]))[design[,op]==1]]
+    samplesref <- row.names(design)[row(as.matrix(design[,ref]))[design[,ref]==1]]
+    
+    colop <- which(colnames(data) %in% samplesop)
+    colref <- which(colnames(data) %in% samplesref)
+    
+    p.value[[i]]<-apply(data,1,function(x){t.test(as.numeric(x[colref]),as.numeric(x[colop]), alternative = "t") $p.value})
+    adjust.p.value[[i]]<-p.adjust(p.value[[i]], method = "BH")
+    
+    fc[[i]]<-rowMeans(data[,colop])-rowMeans(data[,colref])
+    
+    listResults[[i]]<-data.frame(protein=c(row.names(data)))
+    listResults[[i]][paste("p.value.",colnames(contrast)[i],sep="")]=c(p.value[[i]])
+    listResults[[i]][paste("adjust.p.value.",colnames(contrast)[i],sep="")]=c(adjust.p.value[[i]])
+    listResults[[i]][paste("fc.",colnames(contrast)[i],sep="")]=c(fc[[i]])
+    
+  }
+  return(listResults)
+}
+
+
 shinyServer(function(input, output, session) {
   ##rendering the text input to enter the conditions name depending on the number of conditions 
   output$text<-renderUI({
     lapply(1:input$nbCond, function(i) {
-        textInput(paste0('c', i), paste0('name of condition ',i," : "))
+      textInput(paste0('c', i), h5(paste0('name of condition ',i," : ")))
     })
   })
   
   output$select<-renderUI({
     lapply(1:input$nbCond, function(i) {
-      selectizeInput(paste0('r', i), paste0('replicates columns of confitions ',i," : "), choices = list('please choose a dataset'="NULL"),multiple =T )
-      })
+      selectizeInput(paste0('r', i), h5(paste0('replicates columns of confitions ',i," : ")), choices = list('please choose a dataset'="NULL"),multiple =T )
+    })
   }) 
-    
+  
   lcol <- reactiveValues()
   observeEvent(input$dataFile,{
-      lcol$name = colnames(read.table(input$dataFile$datapath, header=T, sep=",",row.names = 1))
-      for(i in 1:input$nbCond){
-        updateSelectizeInput(session, paste0('r', i), choices = setNames(c(1:length(lcol$name)), lcol$name) )
-     }
+    lcol$name = colnames(read.table(input$dataFile$datapath, header=T, sep=",",row.names = 1))
+    for(i in 1:input$nbCond){
+      updateSelectizeInput(session, paste0('r', i), choices = setNames(c(1:length(lcol$name)), lcol$name) )
+    }
   })
   observeEvent(input$nbCond,{
-      for(i in 1:input$nbCond){
-        if (!is.null(input$dataFile)){
-           updateSelectizeInput(session, paste0('r', i), choices = setNames(c(1:length(lcol$name)), lcol$name) )
-        }
-     }
+    for(i in 1:input$nbCond){
+      if (!is.null(input$dataFile)){
+        updateSelectizeInput(session, paste0('r', i), choices = setNames(c(1:length(lcol$name)), lcol$name) )
+      }
+    }
   })
   
-
+  
   ## when the user clicks on the button submit :
   observeEvent(input$submit, {
     
-    
-    dataFile <- input$dataFile
-    nbCond <- input$nbCond
-    
-    updateNumericInput(session, "cond1",max = nbCond)
-    updateNumericInput(session, "cond2",max = nbCond)
-    
-    rep<-list()
-    for(i in 1:nbCond){
-      rep[[i]]<-eval(parse(text = paste0("input$r",i)))
-    }
- 
     ##Hides the data selection panel and show the results 
     hide("dselect")
     show("cselect")
     show("plots")
     
-    ##Setting the names with the values of the text inputs
-    names<-vector()
-    for(i in 1:nbCond){
-      if(eval(parse(text = paste0("input$c",i)))==""){
-        names[i]<- as.character(i)
-      }else {names[i] <- eval(parse(text = paste0("input$c",i)))}
-    }
     
-    if (is.null(dataFile))
-      return(NULL)
-    
+    dataFile <- input$dataFile
     ##loading the file choosed by the user via the file input
     data <- read.table(dataFile$datapath, header=T, sep=",",row.names = 1)
     
     
-    ##applying the DEprot function to the data
-    tryCatch(results <- DEprot(data,nbCond, rep = rep,condName = names,normalization = as.character(input$norm)),
-             warning = function(w){shinyjs::info(w)},
-             error = function(e){
-               if (grepl("essentially constant", as.character(e))){
-                 shinyjs::info("error : the chosen normalization technique makes the data too constant for a student's test. Try to run another normalization.")
-                 show("dselect")
-                 hide("cselect")
-                 hide("plots")
-               }else if (grepl("not enough", as.character(e))){
-                 shinyjs::info("error : please choose the right number of conditions and replicate. \nThere should be at least 2 replicates and two samples")
-                 show("dselect")
-                 hide("cselect")
-                 hide("plots")
-               }else if (grepl("column", as.character(e))){
-                 shinyjs::info(paste0("number of column in the file : ", ncol(data),"\nnumber of samples selected :", (nbCond), "\nplease enter a valid number of samples"))
-                 show("dselect")
-                 hide("cselect")
-                 hide("plots")
-               }else{shinyjs::info(e)}
-             })
+    if(input$manual == FALSE){
+      
+      designFile <- input$designFile
+      design <-as.matrix(read.csv(designFile$datapath, row.names = 1, check.names = F))
+      contrastFile <- input$contrastFile
+      contrast <- as.matrix(read.csv(contrastFile$datapath, row.names = 1, check.names = F))
+      nbComp<-ncol(contrast)
+      updateNumericInput(session, "comp",max = nbComp)
+      
+      norm.data <- norm(data, normalization = input$norm)
+      results <- test.stat(norm.data, design = design, contrast = contrast)
+      
+      
+      #rendering the data frame containing all the results for the selected comparison
+      output$allProteins <- renderDataTable({
+        as.data.frame(results[[input$comp]])
+      })
+      #rendering the button allowing to download all the results for the selected comparison
+      output$dld <- downloadHandler(
+        filename = function(){paste0("DEproteins.",substr(colnames(results[[input$comp]])[2], 9 ,nchar(colnames(results[[input$comp]])[2])), ".csv")},
+        content = function(file) {
+          write.csv(results[[input$comp]], file)
+        }
+      )
+      
+      ##rendering the Volcano plot for the selected comparison
+      ggplotInput <- reactive({
+        plotTitle <- substr(colnames(results[[input$comp]])[2], 9 ,nchar(colnames(results[[input$comp]])[2]))
+        values <- as.data.frame(results[[input$comp]])
+        forplot <- data.frame(x=as.numeric(values[,4]), y=-log10(values[,3]), id=as.character(values[,1]))
+        tmp <- forplot[as.numeric(forplot$y)>=-log10(input$p) & abs(forplot$x)>input$fc,]
+        p <- ggplot(forplot) + geom_point(aes(x, y, label= id , color = ifelse(y>=-log10(input$p) & abs(x)>=input$fc, "not signi", "FC")),show.legend = F) +
+          scale_color_manual(values = c("blue", "red")) +
+          geom_text_repel(data = subset(forplot, abs(forplot$x)>=input$fc & forplot$y>=-log10(input$p)),
+                          aes(x,y,label = id),
+                          size = 4) +
+          geom_vline(xintercept = input$fc ) +
+          geom_vline(xintercept = -input$fc) + 
+          geom_hline(yintercept = -log10(input$p)) + 
+          labs(title = plotTitle,x="log2(Fold-change)", y="-log10(P.Value)") + theme_bw()  
+      })
+      
+      output$volcanoPlot<- renderPlot({
+        print(ggplotInput())
+      })
+      
+      
+      ##rendering the button allowing to download the Volcano plot for the selected comparison
+      output$dlvp <- downloadHandler(
+        filename = function() { paste0("VolcanoPlot-",substr(colnames(results[[input$comp]])[2], 9 ,nchar(colnames(results[[input$comp]])[2])), '.svg')},
+        content = function(file) {
+          plotTitle <- substr(colnames(results[[input$comp]])[2], 9 ,nchar(colnames(results[[input$comp]])[2]))
+          values <- as.data.frame(results[[input$comp]])
+          forplot <- data.frame(x=as.numeric(values[,4]), y=-log10(values[,3]), id=as.character(values[,1]))
+          tmp <- forplot[as.numeric(forplot$y)>=-log10(input$p) & abs(forplot$x)>input$fc,]
+          p <- ggplot(forplot) + geom_point(aes(x, y, label= id , color = ifelse(y>=-log10(input$p) & abs(x)>=input$fc, "not signi", "FC")),show.legend = F) +
+            scale_color_manual(values = c("blue", "red")) +
+            geom_text_repel(data = subset(forplot, abs(forplot$x)>=input$fc & forplot$y>=-log10(input$p)),
+                            aes(x,y,label = id),
+                            size = 2) +
+            geom_vline(xintercept = input$fc ) +
+            geom_vline(xintercept = -input$fc) + 
+            geom_hline(yintercept = -log10(input$p)) + 
+            labs(title = plotTitle,x="log2(Fold-change)", y="-log10(P.Value)") + theme_bw() 
+          ggsave(file,plot = p)
+        }
+      )
+      
+      ##rendering the data frame containing the results for the proteins above the threshold (significant proteins) for the selected comparison
+      output$significantProteins <- renderDataTable({
+        fc = as.data.frame(results[[input$comp]])[,4]
+        p = as.data.frame(results[[input$comp]])[,3]
+        dt<-as.data.frame(results[[input$comp]])
+        if(length(which(p<=input$p & abs(fc)>=input$fc))!=0){
+          return(dt[which(p<=input$p & abs(fc)>=input$fc),])
+        }
+      })
+      ##rendering the button allowing to dowload the results for the proteins above the threshold (significant proteins) for the selected comparison
+      output$dlsd <- downloadHandler(
+        filename = function(){ paste0("significant.DEproteins.",substr(colnames(results[[input$comp]])[2], 9 ,nchar(colnames(results[[input$comp]])[2])), ".csv")},
+        content = function(file) {
+          fc = as.data.frame(results[[input$comp]])[,4]
+          p = as.data.frame(results[[input$comp]])[,3]
+          dt<-as.data.frame(results[[input$comp]])
+          write.csv(dt[which(p<=input$p & abs(fc)>=input$fc),], file)
+        }
+      )
+      
+      output$normalizedPlot <- renderPlot({
+        boxplot(norm.data ,main = "boxplot of the normalized data",ylab="normalized Intensity)", xlab="samples",names=c(1:ncol(data)))
+      })
+      
+    }
     
+    
+    
+    
+    
+    if(input$manual == TRUE) {
+      
+      nbCond <- input$nbCond
+      
+      updateNumericInput(session, "cond1",max = nbCond)
+      updateNumericInput(session, "cond2",max = nbCond)
+      
+      rep<-list()
+      for(i in 1:nbCond){
+        rep[[i]]<-eval(parse(text = paste0("input$r",i)))
+      }
+      
+      
+      
+      ##Setting the names with the values of the text inputs
+      names<-vector()
+      for(i in 1:nbCond){
+        if(eval(parse(text = paste0("input$c",i)))==""){
+          names[i]<- as.character(i)
+        }else {names[i] <- eval(parse(text = paste0("input$c",i)))}
+      }
+      
+      if (is.null(dataFile))
+        return(NULL)
+      
+      
+      
+      
+      ##applying the DEprot function to the data
+      tryCatch(results <- DEprot(data,nbCond, rep = rep,condName = names,normalization = as.character(input$norm)),
+               warning = function(w){shinyjs::info(w)},
+               error = function(e){
+                 if (grepl("essentially constant", as.character(e))){
+                   shinyjs::info("error : the chosen normalization technique makes the data too constant for a student's test. Try to run another normalization.")
+                   show("dselect")
+                   hide("cselect")
+                   hide("plots")
+                 }else if (grepl("not enough", as.character(e))){
+                   shinyjs::info("error : please choose the right number of conditions and replicate. \nThere should be at least 2 replicates and two samples")
+                   show("dselect")
+                   hide("cselect")
+                   hide("plots")
+                 }else if (grepl("column", as.character(e))){
+                   shinyjs::info(paste0("number of column in the file : ", ncol(data),"\nnumber of samples selected :", (nbCond), "\nplease enter a valid number of samples"))
+                   show("dselect")
+                   hide("cselect")
+                   hide("plots")
+                 }else{shinyjs::info(e)}
+               })
+      
       ##rendering the data frame containing all the results for the selected comparison
       output$allProteins <- renderDataTable({
         as.data.frame(results$dataprocessed[input$cond1,input$cond2])
@@ -171,27 +340,6 @@ shinyServer(function(input, output, session) {
           write.csv(results$dataprocessed[input$cond1,input$cond2], file)
         }
       )
-      
-      
-      # normal plot :
-      # output$volcanoPlot <- renderPlot({
-      #   fc = as.data.frame(results$dataprocessed[input$cond1,input$cond2])[,4]
-      #   p = as.data.frame(results$dataprocessed[input$cond1,input$cond2])[,3]
-      #   plot(fc,
-      #        -log10(p),
-      #        main = 'Volcano plot',
-      #        ylab ="-log10(adjusted p value)",
-      #        xlab = paste0("fold change ",names[input$cond1]," vs ",names[input$cond2]),
-      #        abline(h = -log10(input$p), v = c(-input$fc,input$fc)),
-      #        pch = 20,
-      #        col = ifelse(p<=input$p & abs(fc)>=input$fc, "red", "blue"))
-      #    N<-which(p<=input$p & abs(fc)>=input$fc)##threshold
-      #   if(length(N)!=0){
-      #     text(fc[N], -log10(p)[N], labels = as.data.frame(results$dataprocessed[input$cond1,input$cond2])[N,1], cex= 0.5, pos = 2)##adding text on plot
-      #   }
-      # })
-      
-      
       
       ##rendering the Volcano plot for the selected comparison
       ggplotInput <- reactive({
@@ -205,7 +353,7 @@ shinyServer(function(input, output, session) {
           geom_vline(xintercept = input$fc ) +
           geom_vline(xintercept = -input$fc) + 
           geom_hline(yintercept = -log10(input$p)) + 
-          labs(x="log2(Fold-change)", y="-log10(P.Value)") + theme_bw() 
+          labs(title=paste0("VolcanoPlot.",names[input$cond1],".vs.",names[input$cond2]),x="log2(Fold-change)", y="-log10(P.Value)") + theme_bw() 
       })
       
       output$volcanoPlot<- renderPlot({
@@ -217,13 +365,9 @@ shinyServer(function(input, output, session) {
       output$dlvp <- downloadHandler(
         filename = function() { paste0("VolcanoPlot.",names[input$cond1],".vs.",names[input$cond2], '.svg')},
         content = function(file) {
-          
           values <- as.data.frame(results$dataprocessed[input$cond1,input$cond2])
           forplot <- data.frame(x=as.numeric(values[,4]), y=-log10(values[,3]), id=as.character(values[,1]))
           tmp <- forplot[as.numeric(forplot$y)>=-log10(input$p) & abs(forplot$x)>input$fc,]
-          # fc = as.data.frame(results$dataprocessed[input$cond1,input$cond2])[,4]
-          # p = as.data.frame(results$dataprocessed[input$cond1,input$cond2])[,3]
-          # svg(file)
           p <- ggplot(forplot) + geom_point(aes(x, y, label= id , color = ifelse(y>=-log10(input$p) & abs(x)>=input$fc, "not signi", "FC")),show.legend = F) +
             scale_color_manual(values = c("blue", "red")) +
             geom_text_repel(data = subset(forplot, abs(forplot$x)>=input$fc & forplot$y>=-log10(input$p)),
@@ -232,22 +376,8 @@ shinyServer(function(input, output, session) {
             geom_vline(xintercept = input$fc ) +
             geom_vline(xintercept = -input$fc) + 
             geom_hline(yintercept = -log10(input$p)) + 
-            labs(x="log2(Fold-change)", y="-log10(P.Value)") + theme_bw() 
+            labs(title=paste0("VolcanoPlot.",names[input$cond1],".vs.",names[input$cond2]), x="log2(Fold-change)", y="-log10(P.Value)") +theme_bw() 
           ggsave(file,plot = p)
-          
-          # plot(fc,
-          #      -log10(p),
-          #      main = 'Volcano plot',
-          #      ylab ="-log10(adjusted p value)",
-          #      xlab = paste0("fold change ",names[input$cond1]," vs ",names[input$cond2]),
-          #      abline(h = -log10(input$p), v = c(-input$fc,input$fc)),
-          #      pch = 20,
-          #      col = ifelse(p<=input$p & abs(fc)>=input$fc, "red", "blue"))
-          # N<-which(p<=input$p & abs(fc)>=input$fc)
-          # if(length(N)!=0){
-          #   text(fc[N], -log10(p)[N], labels = as.data.frame(results$dataprocessed[input$cond1,input$cond2])[N,1], cex= 0.5, pos = 2)##row(as.matrix(fc))[N]
-          # }
-          # dev.off()
         }
       )
       ##rendering the data frame containing the results for the proteins above the threshold (significant proteins) for the selected comparison
@@ -269,15 +399,20 @@ shinyServer(function(input, output, session) {
           write.csv(dt[which(p<=input$p & abs(fc)>=input$fc),], file)
         }
       )
-    
       
+      output$normalizedPlot <- renderPlot({
+        boxplot(results$normdata ,main = "boxplot of the normalized data",ylab="normalized Intensity)", xlab="samples",names=c(1:ncol(data)))
+      })
+      
+    }
+    
+    
+    
     ##rendering the boxplots to visualize the normalization of the data
     output$unnormalizedPlot <- renderPlot({
       boxplot(log2(data) ,main = "boxplot of the log2 data",ylab="log2(Intensity)", xlab="samples",names=c(1:ncol(data)))
     })
-    output$normalizedPlot <- renderPlot({
-      boxplot(results$normdata ,main = "boxplot of the normalized data",ylab="normalized Intensity)", xlab="samples",names=c(1:ncol(data)))
-    })
+    
     
   })
   
--- ui.R
+++ ui.R
@@ -2,64 +2,91 @@ require(shiny)
 require(shinythemes)
 require(shinyjs)
 shinyUI(fluidPage(theme= shinytheme("superhero"),
-  useShinyjs(),
-  titlePanel("SWATH differential expression"),
-  
-  div(id = "dselect",
-    sidebarPanel(width = 5,
-      fileInput("dataFile", "Choose CSV File",
-                accept=c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
-      "Note: The input file should be a .CSV file , containing a header and the first column being the protein names, 
-      the other columns being the intensity for the differents samples.",
-      br(),
-      br(),
-      
-      fluidRow(
-        column(3, numericInput('nbCond','number of conditions : ',value = 2, min = 2)),
-        column(7, selectInput("norm",  "normalization : ", choices = list('Mean centering and scaling' = "MEAN",
-                                                                          'quantile normalization' = "QUANTILE",
-                                                                          'median centering and scaling' = "MEDIAN",
-                                                                          'no normalization' = "NULL")))
-      ),
-      br(),br(), 
-      
-      fluidRow(
-          column(6, conditionalPanel(condition = "input.nbCond != 0", uiOutput("text"))),
-          column(6, conditionalPanel(condition = "input.nbCond != 0",uiOutput("select")))
-      ),
-          
-      actionButton("submit","Submit")
-    )
-  ),
-
-  hidden(div(id = "cselect",
-    sidebarPanel(
-      h4("choose condition to compare : "),
-      numericInput('cond1','condition 1',value = 1, min = 1),
-      numericInput('cond2','condition 2',value = 2, min = 1),
-    
-      h4("choose threshold : "),
-      sliderInput("p", label = "P value", min = 0, max = 0.2, value = 0.05),
-      sliderInput("fc", label = "Fold change", min = 0, max = 10, value = 0.5, step = 0.1),
-      actionButton("back","back"),
-      
-      width = 2
-    )
-  )),
-
-  mainPanel(
-    hidden(div(id = "plots",
-      tabsetPanel(
-        tabPanel("Volcano plot", plotOutput("volcanoPlot", height = "720px", width = "1280px"),downloadButton('dlvp', 'Download Volcano plot (.svg)')),
-        tabPanel("boxplot", plotOutput("unnormalizedPlot", height = "600px"), plotOutput("normalizedPlot", height = "600px"))
-      ),
-      br(), 
-      
-      tabsetPanel(
-        tabPanel("all proteins", dataTableOutput("allProteins"),downloadButton('dld', 'Download the table with all proteins (.csv)')),
-        tabPanel("significant proteins",dataTableOutput("significantProteins"),downloadButton('dlsd', 'Download the table with selected proteins (.csv)')),
-        br()
-      )
-    ))
-  )
+                  useShinyjs(),
+                  titlePanel("SWATH differential expression"),
+                  
+                  div(id = "dselect",
+                      sidebarPanel(width = 5,
+                                   fileInput("dataFile", h4("Choose CSV File containing the data :"),
+                                             accept=c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+                                   "Note: The input file should be a .CSV file , containing a header and the first column being the protein names, 
+                                   the other columns being the intensity for the differents samples.",
+                                   
+                                   br(),
+                                   
+                                   
+                                   conditionalPanel(
+                                     condition = "input.manual == false",
+                                     fileInput("designFile", h5("Choose CSV File containing the experiment design matrix :"),
+                                               accept=c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+                                     fileInput("contrastFile", h5("Choose CSV File containing the contrast matrix :"),
+                                               accept=c("text/csv", "text/comma-separated-values,text/plain", ".csv"))
+                                   ),
+                                   
+                                   selectInput("norm",  h5("normalization : "), choices = list('Mean centering and scaling' = "MEAN",
+                                                                                               'quantile normalization' = "QUANTILE",
+                                                                                               'median centering and scaling' = "MEDIAN",
+                                                                                               'no normalization' = "NULL")),
+                                   
+                                   checkboxInput("manual","manual entry of the experiment design"),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.manual == true",
+                                     fluidRow(
+                                       column(3, numericInput('nbCond',h5('number of conditions : '),value = 2, min = 2)),
+                                       column(7)
+                                     ),
+                                     br(),
+                                     
+                                     fluidRow(
+                                       column(6, conditionalPanel(condition = "input.nbCond != 0", uiOutput("text"))),
+                                       column(6, conditionalPanel(condition = "input.nbCond != 0",uiOutput("select")))
+                                     )
+                                   ),
+                                   
+                                   
+                                   actionButton("submit","Submit")
+                      )
+                  ),
+                  
+                  hidden(div(id = "cselect",
+                             sidebarPanel(
+                               h4("choose condition to compare : "),
+                               
+                               conditionalPanel(
+                                 condition= "input.manual == true",
+                                 numericInput('cond1','condition 1',value = 1, min = 1),
+                                 numericInput('cond2','condition 2',value = 2, min = 1)
+                               ),
+                               
+                               conditionalPanel(
+                                 condition= "input.manual == FALSE",
+                                 numericInput('comp','comparison',value = 1, min = 1)
+                               ),
+                               
+                               
+                               h4("choose threshold : "),
+                               sliderInput("p", label = "P value", min = 0, max = 0.2, value = 0.05),
+                               sliderInput("fc", label = "Fold change", min = 0, max = 10, value = 0.5, step = 0.1),
+                               actionButton("back","back"),
+                               
+                               width = 2
+                             )
+                  )),
+                  
+                  mainPanel(
+                    hidden(div(id = "plots",
+                               tabsetPanel(
+                                 tabPanel("Volcano plot", plotOutput("volcanoPlot", height = "720px", width = "1280px"),downloadButton('dlvp', 'Download Volcano plot (.svg)')),
+                                 tabPanel("boxplot", plotOutput("unnormalizedPlot", height = "600px"), plotOutput("normalizedPlot", height = "600px"))
+                               ),
+                               br(), 
+                               
+                               tabsetPanel(
+                                 tabPanel("all proteins", dataTableOutput("allProteins"),downloadButton('dld', 'Download the table with all proteins (.csv)')),
+                                 tabPanel("significant proteins",dataTableOutput("significantProteins"),downloadButton('dlsd', 'Download the table with selected proteins (.csv)')),
+                                 br()
+                               )
+                    ))
+                  )
 )) 
